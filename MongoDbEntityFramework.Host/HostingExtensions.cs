9e0bc09b6da4b5e2ca5b62d7bf58a8be752235e8
Solar M. <me@benjmainmacaulay.com>
2025-07-03 22:28:19 +0100

    feat: added host service methods

---
+++ MongoDbEntityFramework.Host/HostingExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MongoDbEntityFramework.Settings;
+
+namespace MongoDbEntityFramework.Host;
+
+public static class HostingExtensions
+{
+    public static void AddMongoDbContext<TContext>(this IServiceCollection services, DbSettings dbSettings)
+        where TContext : DbContext, new()
+    {
+        services.AddSingleton<TContext>(provider => 
+        {
+            TContext context = new();
+            context.Initialize(dbSettings);
+            return context;
+        });
+    }
+    
+}
